bc2436804d4c7a409a7e1ee0fc22b726f0586f67
minh <minhdq1473@gmail.com>
2025-07-18 11:32:33 +0700

    Initial Commit

---
+++ EF25SwiftUIHealthApp/EF25SwiftUIHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EF25SwiftUIHealthAppApp.swift
+//  EF25SwiftUIHealthApp
+//
+//  Created by iKame Elite Fresher 2025 on 18/7/25.
+//
+
+import SwiftUI
+
+@main
+struct EF25SwiftUIHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
